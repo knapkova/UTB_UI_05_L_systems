bcd655a24b26a9b1c17800ce3596c93c38728788
knapkova <118272531+knapkova@users.noreply.github.com>
2025-04-23 12:42:23 +0200

    Add solution

---
+++ 3D.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+from math import sin, cos, radians
+import numpy as np
+
+def lsystem(axiom, rules, iterations, angle, length, output_file):
+    def generate_lsystem(axiom, rules, iterations):
+        words = [axiom]
+        for i in range(iterations):
+            print(i)
+            axiom = ''.join(rules.get(c, c) for c in axiom)
+            words.append(axiom)
+        return words
+
+    def draw_tornado_3d(word, angle, length, ax):
+        x, y, z = 0, 0, 0
+        yaw = 0
+        positions = [(x, y, z)]
+        widths = [5]
+
+        for symbol in word:
+            if symbol == "F":
+                spiral_radius = max(0.5, 10 - z * 0.1)
+                yaw += angle
+                x = spiral_radius * cos(radians(yaw))
+                y = spiral_radius * sin(radians(yaw))
+                z += length * 0.5
+                positions.append((x, y, z))
+                widths.append(max(0.5, widths[-1] * 0.95))
+
+        ax.clear()
+        for i in range(len(positions) - 1):
+            x1, y1, z1 = positions[i]
+            x2, y2, z2 = positions[i + 1]
+            ax.plot([x1, x2], [y1, y2], [z1, z2],
+                    color=plt.cm.Blues(widths[i] / 5),
+                    linewidth=widths[i])
+        ax.view_init(elev=25, azim=45)
+        ax.axis("off")
+
+    words = generate_lsystem(axiom, rules, iterations)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    def update(frame):
+        draw_tornado_3d(words[frame], angle, length, ax)
+
+    anim = FuncAnimation(fig, update, frames=len(words), repeat=False)
+    anim.save(output_file, writer="pillow")
+    plt.close(fig)
+
+rules = {
+    "F": "FF"
+}
+axiom = "F"
+lsystem(axiom, rules, iterations=11, angle=20, length=1.2, output_file="tornado_build.gif")
--- main.py
+++ main.py
@@ -1,48 +1,104 @@
-import turtle
-import random
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from math import sin, cos, radians
 
-
-
-def l_system(axiom, rules, iterations, angle, length):
+def lsystem(axiom, rules, iterations, angle, length, output_file="lsystem.gif"):
     def generate_lsystem(axiom, rules, iterations):
         words = [axiom]
-        for _ in range(iterations):
+        for i in range(iterations):
             axiom = ''.join(rules.get(symbol, symbol) for symbol in axiom)
             words.append(axiom)
+            print(f"Iteration {i + 1}: {axiom}")  # Vypíše slovo pro každou iteraci
         return words
 
-    def draw_lsystem(word, angle, length):
+    def draw_frame(word, angle, length, ax):
         stack = []
+        x, y = 0, 0
+        direction = 0
+        positions = [(x, y)]
         for symbol in word:
             if symbol == "F":
-                turtle.forward(length)
+                x += length * cos(radians(direction))
+                y += length * sin(radians(direction))
+                positions.append((x, y))
             elif symbol == "+":
-                turtle.left(angle)
+                direction += angle
             elif symbol == "-":
-                turtle.right(angle)
+                direction -= angle
             elif symbol == "[":
-                stack.append((turtle.position(), turtle.heading()))
+                stack.append((x, y, direction))
             elif symbol == "]":
-                position, heading = stack.pop()
-                turtle.penup()
-                turtle.goto(position)
-                turtle.setheading(heading)
-                turtle.pendown()
+                x, y, direction = stack.pop()
+                positions.append((x, y))
+        ax.clear()
+        ax.plot(*zip(*positions), color="black")
+        ax.axis("equal")
+        ax.axis("off")
 
     words = generate_lsystem(axiom, rules, iterations)
-    for i, word in enumerate(words):
-        print(f"Iteration {i}: {word}")
-    turtle.speed(0)
-    draw_lsystem(words[-1], angle, length)
-    turtle.done()
+    fig, ax = plt.subplots()
+    ax.axis("equal")
+    ax.axis("off")
+
+    def update(frame):
+        draw_frame(words[frame], angle, length, ax)
+
+    anim = FuncAnimation(fig, update, frames=len(words), repeat=False)
+    anim.save(output_file, writer="pillow")
+    plt.close(fig)
+
+hilbert_rules = {
+    "A": "-BF+AFA+FB-",
+    "B": "+AF-BFB-FA+"
+}
+hilbert_axiom = "A"
+lsystem(
+    axiom=hilbert_axiom,
+    rules=hilbert_rules,
+    iterations=5,
+    angle=90,
+    length=5,
+    output_file="hilbert_curve.gif"
+)
+
+# Example 2: Fractal Plant (Prusinkiewicz)
+plant_rules = {
+    "X": "F-[[X]+X]+F[+FX]-X",
+    "F": "FF"
+}
+plant_axiom = "X"
+lsystem(
+    axiom=plant_axiom,
+    rules=plant_rules,
+    iterations=6,
+    angle=25,
+    length=5,
+    output_file="fractal_plant.gif"
+)
+# Example: Dragon Curve
+dragon_rules = {
+    "X": "X+YF+",
+    "Y": "-FX-Y"
+}
+dragon_axiom = "FX"
+iterations = 10
+angle = 90
+length = 5
+
+lsystem(
+    axiom=dragon_axiom,
+    rules= dragon_rules,
+    iterations = iterations,
+    angle = angle,
+    length = length,
+    output_file="dragon_curve.gif")
 
 rules = {
-    "F": "F[+F]F[-F]F"
+    "F": "FF-[-F+F+F]+[+F-F-F]"
 }
 axiom = "F"
 iterations = 5
 angle = 25
 length = 5
 
-l_system(axiom, rules, iterations, angle, length)
-
+lsystem(axiom, rules, iterations, angle, length, output_file="new.gif")
